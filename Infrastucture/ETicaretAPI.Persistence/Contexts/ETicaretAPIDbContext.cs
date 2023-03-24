210bbe577fb1f210801e0de3f710a52db93ab277
ilhan <ilhan@DESKTOP-J720QN1>
2023-03-25 01:52:14 +0300

    Add project files.

---
+++ Infrastucture/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Contexts
+{
+    public class ETicaretAPIDbContext : DbContext
+    {
+        public ETicaretAPIDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using ETicaretAPI.Application.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+        [HttpGet]
+        public async void Get()
+        {
+            _productWriteRepository.AddRangeAsync(new()
+            {
+                new() { Id = Guid.NewGuid(), Name = "Product1", Stock=100, Price=500,CreatedDate = DateTime.UtcNow  },
+                new() { Id = Guid.NewGuid(), Name = "Product2", Stock=200, Price=600,CreatedDate = DateTime.UtcNow  },
+                new() { Id = Guid.NewGuid(), Name = "Product3", Stock=300, Price=5000,CreatedDate = DateTime.UtcNow  },
+            }
+                );
+            await _productWriteRepository.SaveAsync();
+        }
+    }
+}
